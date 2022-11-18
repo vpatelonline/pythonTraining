44ade2c0968bdddcff27776b15cfbc01e0e4d356
vponline <vpatelonline@gmail.com>
2022-11-17 20:03:01 -0500

    adding method&function script

---
+++ method&functions.py
@@ -0,0 +1,19 @@
+mylist=[1,2,3]
+
+mylist.append(4)
+print(mylist)
+
+mylist.pop()
+print(mylist)
+
+mylist.clear()
+print(mylist)
+
+mylist1=[5,6,7,8,6]
+print(mylist1)
+mylist2 = mylist1.copy()
+print(mylist2)
+
+print(mylist2.count(6))
+
+
