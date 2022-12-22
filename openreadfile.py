d03226087a0a6643f402775e36b7bb088a7f0094
vponline <vpatelonline@gmail.com>
2022-12-22 16:53:33 -0500

    Find folder, sub folder, files

--- openreadfile.py
+++ openreadfile.py
@@ -23,4 +23,18 @@ os.unlink('D:\\IT\\python\\testtextfile.txt')
 
 
 
+for folder, sub_folders,files in os.walk('D:\\Home\\'):
+
+    print(f'Currently looking at {folder}')
+    print('\n')
+
+    print('The subfloders are:')
+    for sub_fold in sub_folders:
+        print(f'\t Sunfolder: {sub_fold}')
+
+    print('\n')
+    print('the filers are:')
+    for f in files:
+        print(f'\t file:{f}')
+    print('\n')
 
