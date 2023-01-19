91b12735bcd58a1ed924587607a4968d0dd9249f
vponline <vpatelonline@gmail.com>
2023-01-19 12:30:22 -0500

    partial speech recognition

--- speechrecognize.py
+++ speechrecognize.py
@@ -5,7 +5,9 @@ myaudio = sr.AudioFile('D:\IT\Python\happybirtday.wav')
 
 with myaudio as source:
 
- audio = r.record(source)
+ audio = r.record(source)   # For full audio to text
+ #audio = r.record(source, duration=1)  - For audio to text up to 1 second - gives error.
+ #audio = r.record(source, duration=1)  - For audio to text up to 2 seconds - It works.
  print(type(audio))
 
  print(r.recognize_google(audio))
