48860abbc51a33fb49f5f190228277f5ceb7bfb9
vponline <vpatelonline@gmail.com>
2023-01-20 12:57:03 -0500

    pylint use

---
+++ pylintuse.py
@@ -0,0 +1,24 @@
+# pylint: disable=C0114
+# keyword and default arguments
+
+# function that calculates cylinder volume
+def cylinder_volume(height, radius=5.0):
+    """Volume of a cylinder
+    Parameters:
+    height (float): Height of the cylinder
+    radius (float): Radius of the cylinder (default 5.0)
+
+    Returns:
+    float:Volume of the cylinder
+
+    """
+    return height * 3.14159 * radius**2
+
+# calling the function using positional arguments
+print(cylinder_volume(10.3, 7.2))
+
+# calling the function using keyword arguments
+print(cylinder_volume(height=10.3, radius=7.2))
+
+#run this script using command - pylint pylintuse.py
+#Your code has been rated at 10.00/10
