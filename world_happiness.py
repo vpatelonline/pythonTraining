142daea49b1110a912f7b50fc4b5207c914f5478
vponline <vpatelonline@gmail.com>
2023-04-30 18:00:19 -0400

    Updated script

--- world_happiness.py
+++ world_happiness.py
@@ -1,11 +1,11 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 import plotly.express as px
 
 happiness = pd.read_csv('world_happiness.csv')
 line_fig = px.line(happiness[happiness['country'] == 'United States'],
                    x='year', y='happiness_score', title='Happiness Score in the United States')
-app=Dash()
+app = Dash()
 
 app.layout = html.Div([
     html.H1('World Happiness Dashboard'),
@@ -16,15 +16,25 @@ app.layout = html.Div([
                    target='_blank')]),
     dcc.RadioItems(options=happiness['region'].unique(),
                    value='North America', inline=True),
-    html.Br(),
     dcc.Checklist(options=happiness['region'].unique(),
                   value=['North America'], inline=True),
-    html.Br(),
-    dcc.Dropdown(options=happiness['country'].unique(),
+    dcc.Dropdown(id='country-dropdown',
+                 options=happiness['country'].unique(),
                  value='United States'),
-    html.Br(),
-    dcc.Graph(figure=line_fig)
+    dcc.Graph(id='happiness-graph')
 ])
+
+
+@app.callback(
+    Output('happiness-graph', 'figure'),
+    Input('country-dropdown', 'value'))
+def update_graph(selected_country):
+    filtered_happiness = happiness[happiness['country'] == selected_country]
+    line_fig = px.line(filtered_happiness,
+                       x='year', y='happiness_score',
+                       title=f'Happiness score in {selected_country}')
+    return line_fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
