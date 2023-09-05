71f3f31ea742a08dee1b2c8defd9dda8e972ec2b
vponline <vpatelonline@gmail.com>
2023-09-05 18:24:29 -0400

    updated live financial dashborad

--- live_financial_dashboard.py
+++ live_financial_dashboard.py
@@ -2,6 +2,7 @@
 from dash import Dash, html, dcc, dash_table
 import plotly.graph_objects as go
 import yfinance as yf
+
 price=yf.Ticker('AAPL').history(period='1d',interval='15m').reset_index()
 fig=go.Figure(data=go.Candlestick(
     x=price['Datetime'],
@@ -21,8 +22,17 @@ app.layout = html.Div([
               placeholder='Search for symbols from Yahoo Finance',
               style={'width': '50%'}),
     html.Button(id='submit-button', children='Submit'),
-    dcc.Graph(id='stock-graph',figure=fig),
-    dash_table.DataTable(id='stock-data',data=price.tail(10).to_dict('records'))
+    html.Br(),
+    html.Br(),
+    dcc.Tabs([
+        dcc.Tab(label='Candlestick Chart', 
+                children=dcc.Graph(id='stock-graph',figure=fig)),
+        dcc.Tab(label='Recent Data',
+                children=dash_table.DataTable(
+                    id='stock-data',
+                    data=price.tail(10).to_dict('records')))
+    ])
+    
 ])
 
 
