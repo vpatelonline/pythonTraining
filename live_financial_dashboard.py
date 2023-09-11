4806b8e244e22b2ca820e7135d549abcbde05661
vponline <vpatelonline@gmail.com>
2023-09-11 12:24:54 -0400

    adding callbacks

--- live_financial_dashboard.py
+++ live_financial_dashboard.py
@@ -1,5 +1,5 @@
 
-from dash import Dash, html, dcc, dash_table
+from dash import Dash, html, dcc, dash_table, Input, Output, State
 import plotly.graph_objects as go
 import yfinance as yf
 
@@ -26,14 +26,37 @@ app.layout = html.Div([
     html.Br(),
     dcc.Tabs([
         dcc.Tab(label='Candlestick Chart', 
-                children=dcc.Graph(id='stock-graph',figure=fig)),
+                children=dcc.Graph(id='stock-graph')),
         dcc.Tab(label='Recent Data',
                 children=dash_table.DataTable(
                     id='stock-data',
                     data=price.tail(10).to_dict('records')))
-    ])
-    
+    ]),
+    dcc.Interval(id='chart-interval', interval=1000*60*15, n_intervals=0),
+    dcc.Interval(id='table-interval', interval=1000*60, n_intervals=0)
 ])
+ 
+@app.callback(Output('stock-graph','figure'),
+              Input('submit-button', 'n_clicks'),
+              Input('chart-interval', 'n_intervals'),
+              State('ticker-input', 'value'))
+def update_chart(button_click, chart_interval, ticker):
+    if ticker is None:
+        return {}
+    else:
+        price=yf.Ticker(ticker).history(period='1d',interval='1m').reset_index()
+        if len(price) > 0:
+            fig = go.Figure(data=go.Candlestick(
+                            x=price['Datetime'],
+                            open=price['Open'],
+                            high=price['High'],
+                            low=price['Low'],
+                            close=price['Close']))
+            return fig
+        else:
+            return {}
+
+
 
 
 if __name__ == '__main__':
